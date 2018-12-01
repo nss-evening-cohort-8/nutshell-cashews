adeeaf427a6aad968e73413c680f7e71375227c2
Robert Rice <rrice1291@gmail.com>
2018-12-01 13:22:01 -0600

    added initializeApp

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import './index.scss';
+import firebase from 'firebase/app';
+
+import apiKeys from '../db/apiKeys.json';
 
 import navbar from './components/navbar/navbar';
 import authHelpers from './components/Helpers/authHelpers';
 
 const initialize = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
   navbar.navBuilder();
   authHelpers.checkLoginStatus();
 };
