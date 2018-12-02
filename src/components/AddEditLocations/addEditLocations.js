5ffa705ca867f1720d1daedfd546d5038448ea38
Austin Cumberlander <austincumberlander@gmail.com>
2018-12-02 12:33:11 -0600

    form appears on load. need to make it appear when add location button is clicked

---
+++ src/components/AddEditLocations/addEditLocations.js
@@ -0,0 +1,26 @@
+import $ from 'jquery';
+
+const formBuilder = () => {
+  const form = `
+  <div class="">
+    <label for="form-zip-code">Zip Code</label>
+    <input type="text" class="form-control" value="" id="form-zip-code" placeholder="Type zip code here...">
+  </div>
+  `;
+  return form;
+};
+
+const printFormToDom = () => {
+  const emptyLocation = {
+    zip_code: '',
+  };
+
+  let domString = '<h2 Add New Location</h2>';
+  domString += formBuilder(emptyLocation);
+  domString += '<button class="btn btn-primary" id="add-location">Save Location</button>';
+  $('#add-edit-location').html(domString).show();
+};
+
+printFormToDom();
+
+export default printFormToDom;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import 'bootstrap';
+import $ from 'jquery';
 import navbar from './components/navbar/navbar';
+import showAddForm from './components/AddEditLocations/addEditLocations';
 import './index.scss';
 
 const initialize = () => {
   navbar.navBuilder();
+  $('#show-location-form').on('click', showAddForm);
 };
 
 initialize();
