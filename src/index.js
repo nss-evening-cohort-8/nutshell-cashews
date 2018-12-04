c0267929f2072c54fec8b8b5e6bed5c32a883f78
Robert Rice <rrice1291@gmail.com>
2018-12-03 20:26:44 -0600

    add

--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@ import navbar from './components/navbar/navbar';
 import authHelpers from './Helpers/authHelpers';
 import loginButton from './components/Auth/auth';
 import printDash from './components/Dashboard/dashboard';
-// import initializeArticlePage from './components/ArticlesPage/articlesPage';
+import initializeArticlePage from './components/ArticlesPage/articlesPage';
 
 const initialize = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   navbar.navBuilder();
   authHelpers.checkLoginStatus(printDash.printDash);
   loginButton.loginButton();
-  // initializeArticlePage.initializeArticlePage();
+  initializeArticlePage.initializeArticlePage();
 };
 
 initialize();
