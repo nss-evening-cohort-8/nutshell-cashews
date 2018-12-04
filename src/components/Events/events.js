56f83eb0a99c875c9e6257308c3dcca34f15c4e7
Colin White <clwhite814@gmail.com>
2018-12-03 21:15:32 -0600

    date is showing on the two events coming from firebase - have to figure out to check for user inputs

--- src/components/Events/events.js
+++ src/components/Events/events.js
@@ -5,12 +5,13 @@ import eventsData from './eventsData';
 const eventsPrinter = (eventsArray) => {
   let eventsCards = '<button type="button" id="add-event-button" class="btn btn-success">Add New Event</button>';
   eventsArray.forEach((event) => {
+    const time = new Date(event.startDate);
     eventsCards += `
         <div class="card d-inline-block m-4" style="width: 18rem;">
   <ul class="list-group list-group-flush">
     <li class="list-group-item">${event.event}</li>
     <li class="list-group-item">${event.location}</li>
-    <li class="list-group-item">${event.startDate}</li>
+    <li class="list-group-item">${time}</li>
   </ul>
 </div>
         `;
